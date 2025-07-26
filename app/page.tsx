1ef7b23efa43e94114c71b1074dd639b6373cfe8
Muhammad Dawood Iqbal <dawoodjand356@gmail.com>
2025-07-26 20:38:01 +0500

    Projects Card and Projects Page Done

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import VideoBar from "./components/VideoBar";
 import About from "./components/About";
+import Projects from "./components/Projects";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Hero />
       <VideoBar />
       <About />
+      <Projects />
     </main>
   );
 }
